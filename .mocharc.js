e8a8445baaecbf34094471af2ea96ff061a42fa8
dobromirtt1 <dobromirtt@gmail.com>
2025-02-17 14:46:19 +0200

    Change 8

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file //some change 7
+// .mocharc.js - Mocha config file //some change 8
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
