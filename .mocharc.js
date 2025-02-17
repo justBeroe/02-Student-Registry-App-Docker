b0492aded6c540b00cece6b500ecaf74fb1148bb
dobromirtt1 <dobromirtt@gmail.com>
2025-02-17 14:39:46 +0200

    test 1 docker

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file //some change 6
+// .mocharc.js - Mocha config file //some change 7
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
